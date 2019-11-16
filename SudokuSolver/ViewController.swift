c18efdf296035e552f700ba6e0ea98c40369ac27
Krzysztof Wolarz <krzysztofwolarz@gmail.com>
2019-11-16 18:35:14 +0100

    Initial Commit

---
+++ SudokuSolver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SudokuSolver
+//
+//  Created by Krzysztof Wolarz on 16/11/2019.
+//  Copyright © 2019 Krzysztof Wolarz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
